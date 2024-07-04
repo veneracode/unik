2479b2f860689cb6a57c149241ccee77482c4500
veneracode <162782787+veneracode@users.noreply.github.com>
2024-07-04 15:50:44 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    int t1, t2, t3;
-    float totalTime, finalTime;
-    // Зчитуємо вхідні дані
-    printf("Введіть три значення : ");
-    scanf("%d %d %d", &t1, &t2, &t3);
+    int p;
+    printf("Enter the number of digits p (p в‰¤ 30): ");
+    scanf("%d", &p);
 
-    // Обчислюємо час в годинах
-    totalTime = (t1+t2+t3)/3.0;
-    // Обчислюємо кінцевий час (час, необхідний для з'їдання пирога)
-    finalTime = totalTime / 3.0;
-    // Виводимо результат з округленням до 2 знаків після коми
-    printf("Час, необхідний для з'їдання пирога: %0.2f годин", finalTime);
+    // Checking input validity
+    if (p < 1 || p > 30) {
+        printf("Invalid value for p. Please enter an integer within the range [1, 30].\n");
+        return 1;  // Return non-zero error code
+    }
+
+    // Calculate the total number of p-digit numbers
+    int totalNumbers = pow(2, p) - (p - 2);
+    printf("Number of %d-digit numbers: %d\n", p, totalNumbers);
+
     return 0;
 }
