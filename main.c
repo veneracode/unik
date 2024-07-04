4cfc64faeaaa096833d9fa4470d1d4c19b1ec695
veneracode <162782787+veneracode@users.noreply.github.com>
2024-07-04 15:42:23 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
 
-int main() {
-    int t1, t2, t3;
-    float totalTime, finalTime;
-    // Зчитуємо вхідні дані
-    printf("Введіть три значення : ");
-    scanf("%d %d %d", &t1, &t2, &t3);
+// Функція для обчислення НСД двох чисел за алгоритмом Евкліда
+int calculateGCD(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return calculateGCD(b, a % b);
+}
 
-    // Обчислюємо час в годинах
-    totalTime = (t1+t2+t3)/3.0;
-    // Обчислюємо кінцевий час (час, необхідний для з'їдання пирога)
-    finalTime = totalTime / 3.0;
-    // Виводимо результат з округленням до 2 знаків після коми
-    printf("Час, необхідний для з'їдання пирога: %0.2f годин", finalTime);
+// Функція для обчислення НСК двох чисел
+int calculateLCM(int a, int b) {
+    int gcd = calculateGCD(a, b);
+    // НСК для двох чисел = (добуток чисел) / (НСД чисел)
+    return (a * b) / gcd;
+}
+
+// Функція для обчислення НСК множини чисел
+int calculateMultipleLCM(int numbers[], int count) {
+    int lcm = numbers[0]; // Ініціалізуємо змінну НСК значенням першого числа
+
+    for (int i = 1; i < count; i++) {
+        lcm = calculateLCM(lcm, numbers[i]);
+    }
+
+    return lcm;
+}
+// Інтерфейс програми
+int main() {
+    int count;
+    printf("Введіть кількість чисел: ");
+    scanf("%d", &count);
+
+    int numbers[count];
+    printf("Введіть числа, розділені пробілом: ");
+    for (int i = 0; i < count; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    int lcm = calculateMultipleLCM(numbers, count);
+    printf("Найменше спільне кратне: %d\n", lcm);
+
     return 0;
 }
