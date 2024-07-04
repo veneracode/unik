5bda988058256d6abbc0c320b3d3eb4842cbbe5e
veneracode <162782787+veneracode@users.noreply.github.com>
2024-07-04 17:01:45 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    int t1, t2, t3;
-    float totalTime, finalTime;
-    // Зчитуємо вхідні дані
-    printf("Введіть три значення : ");
-    scanf("%d %d %d", &t1, &t2, &t3);
-
-    // Обчислюємо час в годинах
-    totalTime = (t1+t2+t3)/3.0;
-    // Обчислюємо кінцевий час (час, необхідний для з'їдання пирога)
-    finalTime = totalTime / 3.0;
-    // Виводимо результат з округленням до 2 знаків після коми
-    printf("Час, необхідний для з'їдання пирога: %0.2f годин", finalTime);
+int main()
+{
+    printf("Hello world!\n");
     return 0;
 }
