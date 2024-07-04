5fff0dda684d0e18fc95668d2631aad42b18e67f
veneracode <162782787+veneracode@users.noreply.github.com>
2024-07-04 16:49:40 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    int t1, t2, t3;
-    float totalTime, finalTime;
-    // Зчитуємо вхідні дані
-    printf("Введіть три значення : ");
-    scanf("%d %d %d", &t1, &t2, &t3);
+    int n;
+    int count = 0; // Counter for equal divisors
 
-    // Обчислюємо час в годинах
-    totalTime = (t1+t2+t3)/3.0;
-    // Обчислюємо кінцевий час (час, необхідний для з'їдання пирога)
-    finalTime = totalTime / 3.0;
-    // Виводимо результат з округленням до 2 знаків після коми
-    printf("Час, необхідний для з'їдання пирога: %0.2f годин", finalTime);
+    // Input and scanning of the input data
+    printf("Enter a natural number n: ");
+    scanf("%d", &n);
+
+    // Checking input validity
+    if (n <= 1 || n >= 150) {
+        printf("Incorrect input. Please enter a natural number n (1 < n < 150).\n");
+        return 0;
+    }
+
+    // Iterate through all possible values of m from 1 to n
+    for (int m = 1; m <= n; m++) {
+        // Check condition n % m == n / m
+        if (n % m == n / m) {
+            count++; // Found an equal divisor, increment the counter
+        }
+    }
+
+    printf("Number of equal divisors of %d: %d\n", n, count);
+
     return 0;
 }
